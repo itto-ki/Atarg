5eb421d5d626b3f6e5f9592930faf2069c0d4f16
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2018-06-21 02:45:43 +0900

    Rename: Package name

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-        name='Atcoder Auto Test Tool',
+        name='Atarg',
         version='0.0.1',
         description='Testing tool before submit for atcoder',
         author='Ittoh Kimura',
         author_email='kimura.itto.kd3@gmail.com',
-        test_suite='atcoder_autotest.tests',
+        test_suite='tests',
         )
