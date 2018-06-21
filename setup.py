bf4048191a92c859f2712a716499533e0cd64acc
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2018-06-21 22:56:28 +0900

    Write settings for packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup, find_packages
+from os import path
+
+root_dir = path.abspath(path.dirname(__file__))
 
 setup(
-        name='Atarg',
+        name='atarg',
         version='0.0.1',
         description='Testing tool before submit for atcoder',
         author='Ittoh Kimura',
         author_email='kimura.itto.kd3@gmail.com',
+        install_requests=[
+            'requests',
+            'beautifulsoup4'
+            ],
+        package_dir={'': 'src'},
+        packages=find_packages(where='src', exclude=('tests')),
+        scripts=['scripts/atarg'],
         test_suite='tests',
         )
