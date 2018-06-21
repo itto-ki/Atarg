945ca82bbd46434d8749e7da8c185264bda9843a
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2018-06-21 23:52:18 +0900

    Add docstrings

--- src/atarg/execute.py
+++ src/atarg/execute.py
@@ -1,8 +1,31 @@
 import subprocess
 import sys
+from typing import List
 
 
-def run_test(test_input, correct_output, command):
+def run_test(
+        test_input: str,
+        correct_output: str,
+        command: List[str]) -> (str, bool):
+    """
+    テストを実行する
+
+    Parameters
+    ----------
+    test_input : str
+        テストのインプット
+    correct_output : str
+        テストの期待する正解アウトプット
+    command : List[str]
+        実行するコマンド
+
+    Returns
+    ----------
+    user_output : str
+        実行したコマンドの出力結果
+    result : bool
+        実行したコマンドの正誤
+    """
     exec_process = subprocess.Popen(command,
                                     stdout=subprocess.PIPE,
                                     stdin=subprocess.PIPE)
@@ -11,7 +34,27 @@ def run_test(test_input, correct_output, command):
     return user_output, user_output == correct_output
 
 
-def run_tests(test_inputs, correct_outputs, command):
+def run_tests(
+        test_inputs: List[str],
+        correct_outputs: List[str],
+        command: List[str]) -> bool:
+    """
+    複数のテストを実行する(実際に実行するのはrun_test)
+
+    Parameters
+    ----------
+    test_inputs : List[str]
+        複数のテストのインプット
+    correct_outputs: List[str]
+        テストの期待する複数のアウトプット
+    command: List[str]
+        実行するコマンド
+
+    Returns
+    ----------
+    result
+        複数のテストの実行結果(1つでも不正解だとFalse)
+    """
     results = []
     for test_input, correct_output in zip(test_inputs, correct_outputs):
         user_output, result = run_test(test_input, correct_output, command)
@@ -21,6 +64,18 @@ def run_tests(test_inputs, correct_outputs, command):
 
 
 def message(test_input, correct_output, user_output, result):
+    """
+    Parameters
+    ----------
+    test_input : str
+        テストのインプット
+    correct_output : str
+        テストの期待する正解アウトプット
+    user_output : str
+        実行したコマンドの出力結果
+    result : bool
+        テストの実行結果
+    """
     sys.stdout.write('Input       : {}\n'.format(repr(test_input)))
     sys.stdout.write('Your Answer : {}\n'.format(user_output))
     sys.stdout.write('Output      : {}\n'.format(correct_output))
--- src/atarg/parse_html.py
+++ src/atarg/parse_html.py
@@ -8,10 +8,53 @@ def fetch_inputs_and_outputs(
         url: str,
         contest: str,
         contest_no: int) -> List[str]:
+    """
+    Atcoderの設問ページから入力と出力を取ってくる
+
+    Parameters
+    ----------
+    url : str
+        設問ページのURL
+    contest : str
+        コンテスト名. ABC, ARC or AGC
+    contest_no : int
+        コンテンストの番号
+
+    Returns
+    ----------
+    inputs_and_output : List[str]
+        インプットとアウトプットが交互に並んだリスト
+    """
     def get_text(html):
+        """
+        HTMLタグ中からテキストを取り出す
+
+        Parameters
+        ----------
+        html
+            BeautifulSoupで定義されたResultSet
+
+        Returns
+        ----------
+        lst
+            取り出されたテキスト文字列のリスト
+        """
         return list(map(lambda tag: tag.get_text().strip(), html))
 
     def split_half(lst):
+        """
+        リストの前半分を取り出す
+
+        Parameters
+        ----------
+        lst
+            半分に分けたいリスト
+
+        Returns
+        ----------
+        lst_half
+            引数で受け取ったリストの前半分
+        """
         return lst[:int(len(lst)/2)]
 
     response = requests.get(url)
@@ -32,6 +75,27 @@ def fetch_inputs_and_outputs(
 
 
 def translate_task(contest: str, contest_no: int, task: str) -> str:
+    """
+    Atcoderは第N回目コンテストのNの値によって、
+    URLに含まれるタスク名がアルファベット(a, b, c, d)表される場合と
+    数字(1, 2, 3, 4)で表される場合がある
+    数字だった場合は、タスク名(A, B, C, D)をアルファベットの小文字に変換、
+    アルファベットだった場合は小文字に変換する
+
+    Parameters
+    ----------
+    contest : str
+        コンテスト名. ABC, ARC or AGC
+    contest_no : int
+        コンテンストの番号
+    task : str
+        タスク名. A, B, C or D
+
+    Returns
+    ----------
+    task
+        タスク名(1, 2, 3, 4 または a, b, c, d)
+    """
     translator = {'A': '1', 'B': '2', 'C': '3', 'D': '4'}
     if contest == 'ABC':
         if 1 <= contest_no <= 19:
@@ -48,6 +112,24 @@ def translate_task(contest: str, contest_no: int, task: str) -> str:
 
 
 def compose_url(contest: str, contest_no: int, task: str) -> str:
+    """
+    与えられたコンテスト名、コンテスト番号、タスク名から
+    正しいURLを生成する
+
+    Parameters
+    ----------
+    contest : str
+        コンテスト名. ABC, ARC or AGC
+    contest_no : int
+        コンテンストの番号
+    task : str
+        タスク名. A, B, C or D
+
+    Returns
+    ----------
+    url
+        対応する問題のURL
+    """
     host = 'https://beta.atcoder.jp/'
     return host + 'contests/' + contest.lower()\
                 + '{:03d}'.format(contest_no)\
