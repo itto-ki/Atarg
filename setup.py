7e93661910991d3d7e08f5a5a176bffa1e706f00
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2019-04-17 09:36:28 +0900

    Fix: description for PyPI

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ root_dir = path.abspath(path.dirname(__file__))
 setup(
         name='atarg',
         version='1.1.0',
-        description='Utilities tools for AtCoder',
+        description='Utility tools for AtCoder',
         author='Ittoh Kimura',
         author_email='kimura.itto.kd3@gmail.com',
         url='https://github.com/itto-ki/Atarg/',
